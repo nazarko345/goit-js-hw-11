a87b3139467ed6ff68528ed9d09875bfa294dbfd
Nazar <nazarijankosuk@gmail.com>
2025-03-18 22:38:29 +0200

    first file changes

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,2 +1,32 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+export const renderHits = (images) => {
+  const markup = images
+    .map(
+      ({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => `
+    <a class="gallery-item" href="${largeImageURL}">
+      <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" />
+        <div class="info">
+          <p><b>Likes:</b> ${likes}</p>
+          <p><b>Views:</b> ${views}</p>
+          <p><b>Comments:</b> ${comments}</p>
+          <p><b>Downloads:</b> ${downloads}</p>
+        </div>
+      </div>
+    </a>`
+    )
+    .join('');
+
+  gallery.innerHTML = markup;
+};
+
+export const lightbox = new SimpleLightbox('.gallery a');
+lightbox.refresh();
+
+export function galleryClean() {
+  gallery.innerHTML = '';
+}
